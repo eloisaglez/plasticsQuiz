ea35615e14f218c13334e76cddf761628e773944
eloisaglez <eloisa.glez.medina@gmail.com>
2026-05-07 21:37:28 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './plasticsQuiz.jsx' // Asegúrate de que aquí el nombre coincida con tu archivo (si pusiste p minúscula)
+import App from './plasticsQuiz.jsx' // <--- Fíjate que aquí he puesto 'p' minúscula
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
