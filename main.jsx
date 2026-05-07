de5072068701b52b964ff0180379a528511af135
eloisaglez <eloisa.glez.medina@gmail.com>
2026-05-07 21:34:26 +0200

    Update import statement comment in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './plasticsQuiz.jsx'
+import App from './plasticsQuiz.jsx' // Asegúrate de que aquí el nombre coincida con tu archivo (si pusiste p minúscula)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
